29e8255f46b1883bf127de678930256611ddab8a
Eben Ash <eben.ash77@gmail.com>
2022-04-18 15:32:11 +0000

    Updates

--- resources/views/homepage/offers.blade.php
+++ resources/views/homepage/offers.blade.php
@@ -21,44 +21,8 @@
                         </span>
                         <span class="thumb-info-caption">
                             <span class="thumb-info-caption-text">
-                                <h4 class="text-uppercase mb-1">Porto Special</h4>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices malesuada ante quis pharetra. Nullam non bibendum dolor. Ut vel turpis accumsan...</p>
-                                <p class="text-right p-4 pb-0">
-                                    <a class="btn btn-primary btn-lg text-2 text-uppercase mb-3" href="{{route('home.reservation')}}">Book Now <i class="icon-calendar icons ml-2"></i></a>
-                                </p>
-                            </span>
-                        </span>
-                    </span>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <span class="thumb-info thumb-info-side-image thumb-info-side-image-custom thumb-info-no-zoom box-shadow-custom">
-                        <span class="thumb-info-side-image-wrapper">
-                            <img alt="" class="img-fluid" style="max-width: 340px;" src="img/demos/hotel/gallery/gallery-1.jpg">
-                        </span>
-                        <span class="thumb-info-caption">
-                            <span class="thumb-info-caption-text">
-                                <h4 class="text-uppercase mb-1">Plan Ahead</h4>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices malesuada ante quis pharetra. Nullam non bibendum dolor. Ut vel turpis accumsan...</p>
-                                <p class="text-right p-4 pb-0">
-                                    <a class="btn btn-primary btn-lg text-2 text-uppercase mb-3" href="{{route('home.reservation')}}">Book Now <i class="icon-calendar icons ml-2"></i></a>
-                                </p>
-                            </span>
-                        </span>
-                    </span>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <span class="thumb-info thumb-info-side-image thumb-info-side-image-custom thumb-info-no-zoom box-shadow-custom mb-0">
-                        <span class="thumb-info-side-image-wrapper">
-                            <img alt="" class="img-fluid" style="max-width: 340px;" src="img/demos/hotel/gallery/gallery-3.jpg">
-                        </span>
-                        <span class="thumb-info-caption">
-                            <span class="thumb-info-caption-text">
-                                <h4 class="text-uppercase mb-1">Valentine's Day</h4>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices malesuada ante quis pharetra. Nullam non bibendum dolor. Ut vel turpis accumsan...</p>
+                                <h4 class="text-uppercase mb-1">No Offers</h4>
+                                <p>There are currently no deals on offer. Please check back later. </p>
                                 <p class="text-right p-4 pb-0">
                                     <a class="btn btn-primary btn-lg text-2 text-uppercase mb-3" href="{{route('home.reservation')}}">Book Now <i class="icon-calendar icons ml-2"></i></a>
                                 </p>
